a8e20dbb04878a04e7be297fb5350a105360f32d
Taylor <tbnelson01@gmail.com>
2023-02-24 16:41:55 -0500

    general layout

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Taylor Nelson</h1>
+      <h1 className="headerName">Taylor Nelson</h1>
       <h3>Front End Developer</h3>
 
-      <div className='teleContainer'>
-        <div className="TV">
-          <p>Test</p>
-
+      <div className="about">
+        <p>My name is Taylor I am a software developer specializing in front end development</p>
       </div>
-    </div>
+
+      <div className='projectContainer'>
+        <div className="projects">
+          <div className="buttonContainer">
+            <h2>Projects</h2>
+            <div className="buttonPanel">
+              <button className="button">Project</button>
+              <button className="button">Project</button>
+              <button className="button">Project</button>
+              <button className="button">Project</button>
+              <button className="button">Project</button>
+              <button className="button">Project</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
 
     </div >
   );
