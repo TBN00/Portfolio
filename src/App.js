109fb3b851f69f55c7032b626173915ab28e6d9e
Taylor <tbnelson01@gmail.com>
2023-02-24 15:46:27 -0500

    getting started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Taylor Nelson</h1>
+      <h3>Front End Developer</h3>
+
+      <div className='teleContainer'>
+        <div className="TV">
+          <p>Test</p>
+
+      </div>
     </div>
+
+    </div >
   );
 }
 
