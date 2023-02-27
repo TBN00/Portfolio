cb2c7f108923323735283ef09c7d686e1b7232b8
Taylor <tbnelson01@gmail.com>
2023-02-27 16:46:07 -0500

    more images in projects

--- src/Components/Journeyal.js
+++ src/Components/Journeyal.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 
 export const Journeyal = () => {
-    const images = ['assets/Journeyal-Login.png', 'assets/Journeyal-Home.png', 'assets/Journeyal-Calendar.png', 'assets/Journeyal-Day.png', 'assets/Journeyal-Search.png', 'assets/Journeyal-Add.png', 'assets/Journeyal-AddExtra.png']
+    const images = ['assets/Journeyal/Journeyal-Login.png', 'assets/Journeyal/Journeyal-Home.png', 'assets/Journeyal/Journeyal-Calendar.png', 'assets/Journeyal/Journeyal-Day.png', 'assets/Journeyal/Journeyal-Search.png', 'assets/Journeyal/Journeyal-Add.png', 'assets/Journeyal/Journeyal-AddExtra.png']
     let [counter, setCounter] = useState(0)
 
 
@@ -26,12 +26,12 @@ export const Journeyal = () => {
     return (
         <div>
             <h2>Journeyal</h2>
-            <img className='projectImages' src={images[counter]}/>
+            <img className='projectImages' alt="images" src={images[counter]}/>
             <div className="directionButtonContainer">
-                <button onClick={() => handleClickLeft()}>
+                <button className="directionButtons" onClick={() => handleClickLeft()}>
                     <p>←</p>
                 </button>
-                <button onClick={() => handleClickRight()}>
+                <button className="directionButtons" onClick={() => handleClickRight()}>
                     <p>→</p>
                 </button>
             </div>
--- src/Components/Trivia.js
+++ src/Components/Trivia.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
+
 export const Trivia = () => {
+    const images = ['assets/Trivia/Trivia-Home.png', 'assets/Trivia/Trivia-Categories.png', 'assets/Trivia/Trivia-Questions.png', 'assets/Trivia/Trivia-QuestionsExtra.png', 'assets/Trivia/Trivia-Score.png']
+    let [counter, setCounter] = useState(0)
+
+
+    const handleClickRight = () => {
+        if(counter === images.length-1){
+            setCounter(0)
+        }
+        else {
+            setCounter(counter+=1)
+        }
+    }
+    const handleClickLeft = () => {
+        if(counter === 0){
+            setCounter(images.length-1)
+        }
+        else {
+            setCounter(counter-=1)
+        }
+    }
+
     return (
         <div>
             <h2>Trivia</h2>
+            <img className='projectImages' alt="images" src={images[counter]}/>
+            <div className="directionButtonContainer">
+                <button className="directionButtons" onClick={() => handleClickLeft()}>
+                    <p>←</p>
+                </button>
+                <button className="directionButtons" onClick={() => handleClickRight()}>
+                    <p>→</p>
+                </button>
+            </div>
         </div>
     );
 }
