12419af980d20721d410c6ed528f73ced775f2bf
Taylor <tbnelson01@gmail.com>
2023-02-28 10:43:31 -0500

    projects component aligned

--- src/App.js
+++ src/App.js
@@ -1,57 +1,32 @@
 import { useState } from 'react';
 import './App.css';
-import {Journeyal} from "./Components/Journeyal"
-import {Ecards} from "./Components/Ecards"
-import {Trivia} from "./Components/Trivia"
-import {Pokedex} from "./Components/Pokedex"
-import {Itunes} from "./Components/Itunes"
-import {Albums} from "./Components/Albums"
-import {Menu} from "./Components/Menu"
+import { About } from './Components/About'
+import { Skills } from './Components/Skills'
+import { Projects } from './Components/Projects'
+import { Menu } from './Components/Menu'
 
 
 function App() {
   const [buttonState, setButtonState] = useState('')
 
-console.log(buttonState)
+  console.log(buttonState)
 
   return (
     <div className="App">
-      <Menu setButtonState={setButtonState}/>
-
-
-
-
-
-      {/* <h1 className="headerName">Taylor Nelson</h1>
-      <h3>Front End Developer</h3>
-
-      <div className="about">
-        <p>My name is Taylor I am a software developer specializing in front end development</p>
+      {buttonState.length === 0 &&
+        <div>
+          <h1>Hi, my name is Taylor</h1>
+          <h2>I'm a front end developer</h2>
+        </div>}
+      <div>
+        {buttonState === 'About' ? <About /> :
+          buttonState === 'Skills' ? <Skills /> :
+            buttonState === 'Projects' ? <Projects /> : null}
       </div>
 
-      <div className='projectContainer'>
-        <div className="projects">
-          <div className="buttonContainer">
-            <h2>Projects</h2>
-            <div className="buttonPanel">
-              <button onClick={() => setButtonState('Journeyal')} className="button">Journeyal</button>
-              <button onClick={() => setButtonState('Ecards')}  className="button">E-Cards</button>
-              <button onClick={() => setButtonState('Trivia')}  className="button">Trivia</button>
-              <button onClick={() => setButtonState('Pokedex')}  className="button">Pokédex</button>
-              <button onClick={() => setButtonState('Itunes')}  className="button">Itunes Search</button>
-              <button onClick={() => setButtonState('Albums')}  className="button">Album Directory</button>
-            </div>
-          </div>
-          <div className="displayProjects">
-          {buttonState === 'Journeyal' ? <Journeyal/> : 
-          buttonState === 'Ecards' ? <Ecards/> :
-          buttonState === 'Trivia' ? <Trivia/> :
-          buttonState === 'Pokedex' ? <Pokedex/> :
-          buttonState === 'Itunes' ? <Itunes/> :
-          buttonState === 'Albums' ? <Albums/> : null}
-          </div>
-        </div>
-      </div> */}
+
+      <Menu setButtonState={setButtonState} />
+
 
     </div >
   );
---
+++ src/Components/Projects.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Journeyal } from "./Journeyal";
+import { Ecards } from "./Ecards";
+import { Trivia } from "./Trivia";
+import { Pokedex } from "./Pokedex";
+import { Itunes } from "./Itunes";
+import { Albums } from "./Albums";
+
+
+export const Projects = () => {
+    const [projectState, setProjectState] = useState('Journeyal')
+
+
+    return (
+        <div>
+            <div className='projectContainer'>
+                <div className="projects">
+                    <div className="buttonContainer">
+                        <h2>Projects</h2>
+                        <div className="buttonPanel">
+                            <button onClick={() => setProjectState('Journeyal')} className="button">Journeyal</button>
+                            <button onClick={() => setProjectState('Ecards')} className="button">E-Cards</button>
+                            <button onClick={() => setProjectState('Trivia')} className="button">Trivia</button>
+                            <button onClick={() => setProjectState('Pokedex')} className="button">Pokédex</button>
+                            <button onClick={() => setProjectState('Itunes')} className="button">Itunes Search</button>
+                            <button onClick={() => setProjectState('Albums')} className="button">Album Directory</button>
+                        </div>
+                    </div>
+                    <div className="displayProjects">
+                        {projectState === 'Journeyal' ? <Journeyal /> :
+                            projectState === 'Ecards' ? <Ecards /> :
+                            projectState === 'Trivia' ? <Trivia /> :
+                            projectState === 'Pokedex' ? <Pokedex /> :
+                            projectState === 'Itunes' ? <Itunes /> :
+                            projectState === 'Albums' ? <Albums /> : null}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
