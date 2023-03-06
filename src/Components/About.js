26caa383f69eb17b0505192f7ccfb95fa20ab0f3
Taylor <tbnelson01@gmail.com>
2023-03-06 14:02:07 -0500

    working transitions

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const About = () => {
     let [counter, setCounter] = useState(0)
     let pictures = ["/assets/About/KY9A0911.jpg", "/assets/About/IMG_1214.jpg", "/assets/About/IMG_1215.png", "/assets/About/IMG_7551.jpg", "/assets/About/IMG_7681.jpg"]
     let timerRef
 
-    function timer () {
-        if (counter === pictures.length - 1){
+    function timer() {
+        if (counter === pictures.length - 1) {
             setCounter(0)
         }
         else {
@@ -17,21 +17,34 @@ export const About = () => {
     clearInterval(timerRef)
     timerRef = setTimeout(timer, 4000)
 
+
     return (
         <div className="projectContainer">
-            <div style={{ width: '80%' }}>
-                <img style={{height: "50vh"}} src={pictures[counter]}/>
-                <h1>Hi, my name is Taylor Nelson</h1>
-                <br />
-                <h2>
-                    With years of experience in supply chain logistics and management,
-                    I decided to take a huge leap and go for my dream career. As a recent
-                    graduate in the Momentum program, I've had an amazing time learning JavaScript,
-                    Python, Django and React. I've developed a passion for front end development which
-                    led me to branch into React Native. Collaborating with others to create a mobile
-                    app has been some of the most fulfilling work I've ever done. I'm aspiring to
-                    bring a broad background with strong technical skills to excel as a Front-End Developer.
-                </h2>
+            <div style={{ width: '80%', zIndex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                {pictures.map((pic, index) => (
+                    <img
+                        id={index}
+                        key={index}
+                        className={index === counter ? "image active" : "image"}
+                        src={pic}
+                        style={{
+                            zIndex: `-${index + 1}`
+                        }}
+                    />
+                ))}
+                <div style={{marginTop: "50vh"}}>
+                    <h1>Hi, my name is Taylor Nelson</h1>
+                    <br />
+                    <h2>
+                        With years of experience in supply chain logistics and management,
+                        I decided to take a huge leap and go for my dream career. As a recent
+                        graduate in the Momentum program, I've had an amazing time learning JavaScript,
+                        Python, Django and React. I've developed a passion for front end development which
+                        led me to branch into React Native. Collaborating with others to create a mobile
+                        app has been some of the most fulfilling work I've ever done. I'm aspiring to
+                        bring a broad background with strong technical skills to excel as a Front-End Developer.
+                    </h2>
+                </div>
             </div>
         </div>
     );
